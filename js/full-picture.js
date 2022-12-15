d7863655c72371412906b9ea5c3028e8b6ebd119
germiJkz <103726508+germiJkz@users.noreply.github.com>
2022-12-16 01:13:10 +0500

    8модуль2задание

--- js/full-picture.js
+++ js/full-picture.js
@@ -1,27 +1,7 @@
+import { setComments } from './comments.js';
+
 const bigPicture = document.querySelector('.big-picture');
-const commentsBigPicture = bigPicture.querySelector('.social__comments');
 const closeButton = bigPicture.querySelector('.big-picture__cancel');
-
-const createCommentItem = (commentData) => {
-  const liElement = document.createElement('li');
-  const imgElement = document.createElement('img');
-  const pElement = document.createElement('p');
-
-  imgElement.classList.add('social__picture');
-  imgElement.src = commentData.avatar;
-  imgElement.alt = commentData.name;
-  imgElement.width = 35;
-  imgElement.height = 35;
-
-  pElement.classList.add('social__text');
-  pElement.textContent = commentData.message;
-
-  liElement.classList.add('social__comment');
-  liElement.appendChild(imgElement);
-  liElement.appendChild(pElement);
-
-  return liElement;
-};
 
 const onCloseButtonClick = () => {
   bigPicture.classList.add('hidden');
@@ -44,18 +24,10 @@ const onPictureClick = (pictureData) => {
 
   bigPicture.querySelector('.big-picture__img').querySelector('img').src = pictureData.url;
   bigPicture.querySelector('.likes-count').textContent = pictureData.likes;
-  bigPicture.querySelector('.comments-count').textContent = pictureData.comments.length;
+
   bigPicture.querySelector('.social__caption').textContent = pictureData.description;
 
-  const lastComments = commentsBigPicture.querySelectorAll('.social__comment');
-  lastComments.forEach((lastComment) => {commentsBigPicture.removeChild(lastComment);});
-
-  pictureData.comments.forEach((comment) => {
-    commentsBigPicture.appendChild(createCommentItem(comment));
-  });
-
-  bigPicture.querySelector('.social__comment-count').classList.add('hidden');
-  bigPicture.querySelector('.comments-loader').classList.add('hidden');
+  setComments(pictureData.comments);
   document.querySelector('body').classList.add('modal-open');
 
   closeButton.addEventListener('click', onCloseButtonClick);
